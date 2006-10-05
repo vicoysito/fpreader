7d1f1a099e4e7bbb1cfa59b9317982136053bd5c
teonivalois <teonivalois@0257399f-0e1f-0410-9f4d-a5aebe7c764b>
2006-10-05 17:06:33 +0000

    Initial import

---
+++ trunk/ fpreader --username teonivalois/src/br/com/teoni/fingerprintreader/model/FP.java
@@ -0,0 +1,27 @@
+/*
+ * FP.java
+ *
+ * Created on 25 de Setembro de 2006, 19:59
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package br.com.teoni.fingerprintreader.model;
+
+/**
+ *
+ * @author Teoni
+ */
+public class FP {
+    
+    private int height;
+    private int width;
+    private String url;
+    private byte[][] skeleton;
+    
+    /** Creates a new instance of FP */
+    public FP() {
+    }
+    
+}
